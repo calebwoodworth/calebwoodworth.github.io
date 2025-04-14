8ecffbb60df6d826c9599058713ba98a45a6e766
calebwoodworth <caleb.woodworth@gmail.com>
2025-04-14 14:22:13 -0400

    Add images

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
-    $("#header").load("includes/header.html");
-    $("#footer").load("includes/footer.html");
-  
-    $("#thumbnailList a").on("click", function (e) {
+    $('#thumbnailList a').on('click', function (e) {
       e.preventDefault();
-      const imgFile = $(this).data("img");
-      const captionText = $(this).data("caption");
-      $("#mainImage").attr("src", "images/" + imgFile);
-      $(".caption").text(captionText);
+  
+      const newImg = $(this).data('img');
+      const newCaption = $(this).data('caption');
+  
+      $('#mainImage').attr('src', 'images/' + newImg).attr('alt', newCaption);
+      $('.caption').text(newCaption);
     });
+  
+  
+    $('#header').load('components/header.html');
+    $('#footer').load('components/footer.html');
   });
   
